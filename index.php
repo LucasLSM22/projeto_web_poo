0522fa6163ecd952d77d0c63c5b3b0be7956a751
Lucas Marçal <126588175+LucasLSM22@users.noreply.github.com>
2023-03-07 12:11:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     </head>
     <body>
         <form method="POST" action="teste.class.php">
-            Nome: <input type="text" name="nome" id="nome"> <br>
-            Endereço: <input type="text" name="endereco" id="endereco"> <br>
-            Bairro: <input type="text" name="bairro" id="bairro"> <br>
-            CEP: <input type="text" name="cep" id="cep"> <br>
-            Cidade: <input type="text" name="cidade" id="cidade"> <br>
+            Nome: <input type="text" name="nome" id="nome"> <br> <br>
+            Endereço: <input type="text" name="endereco" id="endereco"> <br> <br>
+            Bairro: <input type="text" name="bairro" id="bairro"> <br> <br>
+            CEP: <input type="text" name="cep" id="cep"> <br> <br>
+            Cidade: <input type="text" name="cidade" id="cidade"> <br> <br>
             Estado: <input type="text" name="estado" id="estado"> <br><br>
             <input type="submit" name="Cadastrar" value="Cadastrar">
         </form>
